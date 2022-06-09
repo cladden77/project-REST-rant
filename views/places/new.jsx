0fe2fde6658e400d156814e52483fba68a3ca3fa
Chris L <chris.ladden@gmail.com>
2022-06-09 18:34:11 -0500

    Adding and rendering a new view

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form () {
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                <form method="POST" action="/places">
+                    <div>
+                        <label htmlFor="name" required>Place Name</label>
+                        <input id="name" name="name" />
+                    </div>
+                    <div>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id="pic" name="pic" />
+                    </div>
+                    <div>
+                        <label htmlFor="city">City</label>
+                        <input id="city" name="city" />
+                    </div>
+                    <div>
+                        <label htmlFor="state">State</label>
+                        <input id="state" name="state" />
+                    </div>
+                    <div>
+                        <label htmlFor="cuisines" required>Cuisines</label>
+                        <input id="cuisines" name="cuisines" />
+                    </div>
+                    <input type="submit" value="Add Place" />
+                </form>
             </main>
         </Def>
     )
