9ab00d798b0aab2a661ad50253d5b74e84d71036
Chris L <chris.ladden@gmail.com>
2022-06-09 19:12:10 -0500

    added stylesheets and updated 404

--- views/error404.jsx
+++ views/error404.jsx
@@ -4,9 +4,18 @@ const Def = require('./default')
 function error404 () {
     return (
       <Def>
-          <main>
+          <main class="text-center">
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+              <img src="/images/404-cone.jpg" alt="melted ice cream cone" />
+                <div>
+                Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&#38;utm_medium=referral&#38;utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/?utm_source=unsplash&#38;utm_medium=referral&#38;utm_content=creditCopyText">Unsplash</a>
+                </div>
+                </div>
+              <a href="/places">
+              <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,15 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/food-img.jpg" alt="chia Fruit Drink" />
+                <div>
+                  Photo by <a href="https://unsplash.com/photos/MsTOg6rhRVk#:~:text=Photo%20by-,Brenda%20Godinez,-on%20Unsplash">Brenda Godinez</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+                </div>
               <a href="/places">
               <button className="btn-primary">Places Page</button>
               </a>
-
           </main>
       </Def>
     )
