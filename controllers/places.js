ed4419db67aa501cd7c897f8c136c3f5a73bb3ec
Chris L <chris.ladden@gmail.com>
2022-06-09 20:36:35 -0500

    Form data is coming through

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,11 @@
 const React = require('react')
 const express = require('express');
+const { Router } = require('express');
+const res = require('express/lib/response');
 const app = express();
+//const places = require('../models/places.js')
+const router = require('express').Router()
+
 
 app.get('/', (req, res) => {
     let places = [{
@@ -16,14 +21,19 @@ app.get('/', (req, res) => {
         cuisines: 'Coffee, Bakery',
         pic: 'http://placekitten.com/250/250'
       }]
-      
+
     res.render('places/index', { places })
 })
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
 
 // New Place
 app.get('/new', (req, res) => {
   res.render('places/new')
 })
-
 
 module.exports = app
