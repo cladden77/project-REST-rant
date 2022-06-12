d25ea810544fab2d597ec46be5e3de5a98b91fb6
Chris L <chris.ladden@gmail.com>
2022-06-12 11:18:17 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -36,11 +36,27 @@ app.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
-  } else if (!places[id]){
+  }
+  else if (!places[id]) {
     res.render('error404')
   }  
   else {
-    res.render('places/show', { place: places[id]})
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
+//Delete Route
+router.delete('/places/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }  
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
 
