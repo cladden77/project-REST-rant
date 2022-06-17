9773454b4295f239f1ed15aacd163b5c46150a58
Chris L <chris.ladden@gmail.com>
2022-06-16 19:08:31 -0500

    refactured and added database connection

--- controllers/places.js
+++ controllers/places.js
@@ -5,86 +5,72 @@ const res = require('express/lib/response');
 const app = express();
 const places = require('../models/places.js')
 const router = require('express').Router()
+const db = require('../models')
 
 
-app.get('/', (req, res) => {
-    res.render('places/index', { places })
+router.get('/', (req, res) => {
+  db.Place.find()
+      .then((places) => {
+          res.render('places/index', { places })
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
 })
 
-app.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+router.post('/', (req, res) => {
+  if (req.body.pic === '') { req.body.pic = undefined }
+  if (req.body.city === '') { req.body.city = undefined }
+  if (req.body.state === '') { req.body.state = undefined }
+  db.Place.create(req.body)
+      .then(() => {
+          res.redirect('/places')
+      })
+      .catch(err => {
+        console.log('err', err)
+          res.render('error404')
+      })
 })
 
 // New Place
-app.get('/new', (req, res) => {
+router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+// Show Route
+router.get('/:id', (req, res) => {
+  db.Place.findOne({ _id: req.params.id })
+      .then(place => {
+          res.render('places/show', { place })
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
+})
+
+//Single route
+router.put('/:id', (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+      .then(() => {
+          res.redirect(`/places/${req.params.id}`)
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
+})
+
 // Edit Route
-app.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id], id })
-  }
-})
-
-//Show Route
-app.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }  
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-// PUT Route
-app.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+router.get('/:id/edit', (req, res) => {
+  db.Place.findById(req.params.id)
+      .then(place => {
+          res.render('places/edit', { place })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
 })
 
 //Delete Route
@@ -98,8 +84,18 @@ router.delete('/places/:id', (req, res) => {
   }  
   else {
     places.splice(id, 1)
-    res.redirect('/places')
+    res.send('DELETE /places/:id stub')
   }
 })
 
-module.exports = app
+//RANT route
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+// Delete Rant
+router.delete('/:id/rant/rantId', (req, res) => {
+  res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
