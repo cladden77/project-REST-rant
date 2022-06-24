26c6469f9f3e40f79a6edfead1eb4612ce3d8260
Chris L <chris.ladden@gmail.com>
2022-06-23 21:16:10 -0500

    add comment functionality and keep within database

--- controllers/places.js
+++ controllers/places.js
@@ -18,27 +18,25 @@ router.get('/', (req, res) => {
       })
 })
 
-router.post('/', (req, res) => {
-  if (req.body.city === '') { req.body.city = undefined }
-  if (req.body.state === '') { req.body.state = undefined }
+router.post("/", (req, res) => {
   db.Place.create(req.body)
-      .then(() => {
-          res.redirect('/places')
-      })
-      .catch(err => {
-        if (err && err.name == 'ValidationError') {
-          let message = 'Validation Error: '
-          for (var field in err.errors) {
-            message += `${field} was ${err.errors[field].value}. `
-            message += `${err.errors[field].message}`
-          }
-          res.render('places/new', { message })
-        }
-          else { 
-            res.render('error404')
-      }
+    .then(() => {
+      res.redirect("/places");
     })
-})
+    .catch((err) => {
+      if (err && err.name == "ValidationError") {
+        let message = "Validation Error: ";
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `;
+          message += `${err.errors[field].message}`;
+        }
+        console.log("Validation error message", message);
+        res.render("places/new", { message });
+      } else {
+        res.render("error404");
+      }
+    });
+});
 
 // New Place
 router.get('/new', (req, res) => {
@@ -48,7 +46,9 @@ router.get('/new', (req, res) => {
 // Show Route
 router.get('/:id', (req, res) => {
   db.Place.findOne({ _id: req.params.id })
+  .populate('comments')
       .then(place => {
+        console.log(place.comments)
           res.render('places/show', { place })
       })
       .catch(err => {
@@ -92,6 +92,33 @@ router.delete('/places/:id', (req, res) => {
     res.send('DELETE /places/:id stub')
   }
 })
+
+// Comment route
+router.post("/:id/comment", (req, res) => {
+  if (req.body.rant) {
+    req.body.rant = true;
+  } else {
+    req.body.rant = false;
+  }
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      console.log(db.Comment);
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
 //RANT route
 router.post('/:id/rant', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,23 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className="inactive">No comments yet!</h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className='rant'>{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
         <Def>
           <main>
@@ -37,7 +54,31 @@ function show (data) {
               <div className="row justify-content-center mt-5 border-top">
                 <div className="col pt-3">
                 <h2>Comments</h2>
-                <p>No Comments Yet!</p>
+                {comments}
+
+                <form method='POST' action={`/places/${data.place.id}/comment`}>
+                  <div className='form-group'>
+                    <label htmlFor='author'>Author</label>
+                    <input className='form-control' id='author' name='author'/>
+                  </div>
+
+                  <div className='form-group'>
+                    <label htmlFor='content'>Content</label>
+                    <input className='form-control' id='content' name='content' type='textarea' />
+                  </div>
+
+                  <div className='form-group'>
+                    <label htmlFor='stars'>Star Rating</label>
+                    <input className='form-control' id='stars' name='stars' type='range' step='0.5' min='0' max='5' />
+                  </div>
+
+                  <div className='form-group'>
+                    <label htmlFor='rant'>Rant</label>
+                    <input  id='rant' name='rant' type='checkbox' defaultChecked/>
+                  </div>
+
+                  <input className='btn btn-primary' type='submit' value='Add Comment' />
+                </form>
                 </div>
               </div>
             </div>
